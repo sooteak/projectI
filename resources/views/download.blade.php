52d2ef42606c3ee5b462761b7f0620c42388a3b9
sooteak <kids.st2001@gmail.com>
2022-01-13 16:39:18 +0800

    update

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -3,12 +3,13 @@
 <html>
     <body style="border-radius: 5px;background:#7979798f;margin-top:100px;padding:20px;margin-bottom: 100px;text-align:center;">
         <h5><strong>Download Resources</strong></h5><br>
-        <p><strong>SUC Intro</strong></p>
-        <a href="/file/SUC Intro 2021.pdf" download><button type="button">SUC Intro 2021</button></a><hr>
+        <p><strong>Freshmen Notice</strong></p>
+        <a href="/file/Freshmen Guide.pdf" download><button type="button">Freshmen Guide</button></a><hr>
+        <a href="/file/Rules and Regulations of Student Societies.pdf" download><button type="button">Rules and Regulations od Student Societies</button></a><hr>
+        <a href="/file/SUC Intro 2021" download><button type="button">SUC Intro 2021</button></a><hr>
         <p><strong>Fee</strong></p>
-        <a href="/file/International Fees Structure.pdf" download><button type="button">International Fees Structure</button></a><br><hr>
+        <a href="/file/International Student Program Fees & Procedures vAugust.pdf" download><button type="button">International Fees Structure</button></a><br><hr>
         <p><strong>PTPTN</strong></p>
-        <a href="/file/FEP -PTPTN Application.pdf" download><button type="button">PTPTN Application</button></a><br>
         <a href="/file/The Process of PTPTN Application.pdf" download><button type="button">The Process of PTPTN Application</button></a><hr>
         <p><strong>Faculty Details</strong></p>
         <a href="/file/FAD- Faculty of Art and Design.pdf" download><button type="button">FAD- Faculty of Art and Design</button></a><br>
@@ -18,14 +19,13 @@
         <a href="/file/FEIT- Faculty of Engineering and Information Technology VERSION 2.pdf" download><button type="button">FEIT- Faculty of Engineering and Information Technology</button></a><br>
         <a href="/file/FEP -Faculty of Education & Psychology 2021 v5.pdf" download><button type="button">FEP- Faculty of Education & Psychology 2021</button></a><br>
         <a href="/file/FHSS- Faculty of Humanities and Social Sciences Version 2.pdf" download><button type="button">FHSS- Faculty of Humanities and Social Sciences</button></a><br>
-        <a href="/file/FEP -postgraduate brochures 2021 VERSION 2.pdf" download><button type="button">FEP- postgraduate brochures 2021</button></a><br>
-        <a href="/file/FEP -SFS-School of Foundation Studies.pdf" download><button type="button">SFS-School of Foundation Studies</button></a><br>
-        <a href="/file/FEP -Programme Guide 2021 VERSION 3.pdf" download><button type="button">All Programme Details 2021</button></a><hr>
-        <p><strong>Insurance</strong></p>
-        <a href="/file/FEP -Student Insurance (ENG).pdf" download><button type="button">Student Insurance (ENG)</button></a><br>
-        <a href="/file/学生保险 (CN)" download><button type="button">Student Insurance 学生保险(CN)</button></a><hr>
-        <p><strong>Scholarship</strong></p>
-        <a href="/file/FEP -Scholarship Application Form.pdf" download><button type="button">Scholarship Application Form</button></a><br><hr>
+        <a href="/file/postgraduate brochures 2021 VERSION 2.pdf" download><button type="button">postgraduate brochures 2021</button></a><br>
+        <a href="/file/SFS- School of Foundation Studies.pdf" download><button type="button">SFS-School of Foundation Studies</button></a><br>
+        
+        <p><strong>General Q&A</strong></p>
+        <a href="/file/General Q&A.pdf" download><button type="button">General Q&A</button></a><br>
+        <p><strong>Scholarship Application Form</strong></p>
+        <a href="/file/Scholarship Application Form.pdf" download><button type="button">Scholarship Application Form</button></a><br><hr>
         <p><strong>Student Hostel</strong></p>
         <a href="/file/hostel.pdf" download><button type="button">Hostel</button></a><br>
         <a href="/file/Student Residence Application Form.pdf" download><button type="button">Student Residence Application Form</button></a><br><hr>
